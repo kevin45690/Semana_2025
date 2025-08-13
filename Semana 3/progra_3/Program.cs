c96335bd0bf1992938dbc4c85439bf45f9b141e1
kevin45690 <fn0446032023@unab.edu.sv>
2025-08-13 09:52:23 -0600

    Add files via upload

---
+++ Semana 3/progra_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using progra_3;
+using System.Drawing;
+var echo1 = utileriaGenerica.Echo(34);
+var echo2 = utileriaGenerica.Echo("proramacion |||");
+
+int x = 3;
+string resultado = x > 0 ? "Positivo" : "negativo";
+if (x > 0)
+    resultado = "Positivo";
+else
+    resultado = "negativo";
+
+    Console.WriteLine($"Echo int: {echo1}");
+Console.WriteLine($"Echo string: {echo2}");
+Console.WriteLine($"5 es igual que 5:{utileriaGenerica.Esigual(5, 5)}");
+Console.WriteLine($"A es igual que B:{utileriaGenerica.Esigual("A", "A")}");
+Console.WriteLine($"Max entre 7 y 5:{utileriaGenerica.Max(7, 5)}");
+var repetirEnteros = utileriaGenerica.Repetir(10, 6);
+Console.WriteLine($"repeticion de Enteros: {string.Join(",", repetirEnteros)}");
+Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+void MostrarInformacion<T>(T Valor)
+{
+    Console.WriteLine($"valor recibido: {Valor}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ Semana 3/progra_3/utileriaGenerica.cs
@@ -0,0 +1,30 @@
+﻿namespace progra_3
+{
+    public static class utileriaGenerica
+    {
+        public static T Echo<T>(T valor)
+        {
+            return valor;
+        }
+        public static bool Esigual<T>(T primerValor, T segundoValor)
+        {
+            return EqualityComparer<T>.Default.Equals(primerValor, segundoValor);
+        }
+        public static T Max<T>(T primerValor, T segundoValor) where T : IComparable<T>
+        {
+            return primerValor.CompareTo(segundoValor)
+                >= 0 ? primerValor : segundoValor;
+
+
+        }
+        public static List<T> Repetir<T>(T valor, int Repetir) 
+        {
+                var list = new List<T>();
+            for (int i = 0; i < Repetir; i++)
+                list.Add(valor);
+            return list;
+
+        }    
+    }
+}   
+
